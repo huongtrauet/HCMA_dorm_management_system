1c9828edd80fcb692a0222b2def7df22fb4671d8
huongtrauet <huongtrauet@gmail.com>
2021-05-29 13:15:51 +0700

    fix puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -1,27 +1,26 @@
-
 # Change to match your CPU core count
 workers 1
 
-# # Min and Max threads per worker
-# threads 1, 6
+# Min and Max threads per worker
+threads 1, 6
 
 app_dir = "/home/ubuntu/HCMA_dorm_management_system"
 shared_dir = "#{app_dir}/tmp"
 
-# # Default to production
-# rails_env = ENV['RAILS_ENV'] || "production"
-# environment rails_env
+# Default to production
+rails_env = ENV['RAILS_ENV'] || "production"
+environment rails_env
 
-# # Set up socket location
-# bind "unix://#{shared_dir}/sockets/puma.sock"
+# Set up socket location
+bind "unix://#{shared_dir}/sockets/puma.sock"
 
-# # Logging
-# stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+# Logging
+stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 
-# # Set master PID and state locations
-# pidfile "#{shared_dir}/pids/puma.pid"
-# state_path "#{shared_dir}/pids/puma.state"
-# activate_control_app
+# Set master PID and state locations
+pidfile "#{shared_dir}/pids/puma.pid"
+state_path "#{shared_dir}/pids/puma.state"
+activate_control_app
 
 on_worker_boot do
   require "active_record"
