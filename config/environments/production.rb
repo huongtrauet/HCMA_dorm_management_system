5b385bfd1c0556fad9e63049b1f6ddf40f27df6a
huongtrauet <huongtrauet@gmail.com>
2021-06-01 23:03:43 +0700

    update gmail password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,7 @@ Rails.application.configure do
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :user_name            => "huongtranguyen2111@gmail.com",
-    :password             => "Hoidelamgi2111",
+    :password             => "ykjzrvwukofzcbiz",
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
